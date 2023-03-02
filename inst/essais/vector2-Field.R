219131fb1fc3422f65d5ec9ed9ee296ac5453319
Stéphane Laurent <laurent_step@yahoo.fr>
2023-03-02 16:46:00 +0100

    plot vector field

---
+++ inst/essais/vector2-Field.R
@@ -0,0 +1,17 @@
+x <- y <- c(-5, -4, -3, -2, 2, 3, 4, 5)
+From <- as.matrix(expand.grid(x0 = x, y0 = y))
+f <- function(x0y0) {
+  d <- 2 * sqrt(c(crossprod(x0y0)))
+  x0y0 - x0y0 / d
+  # x0 <- x0y0[1L]; y0 <- x0y0[2L]
+  # x1 <- -x0/d; y1 <- -y0/d
+  # c(x1, y1)
+}
+To <- t(apply(From, 1L, f))
+x0 <- From[, "x0"]; y0 <- From[, "y0"]
+x1 <- To[, 1L]; y1 <- To[, 2L]
+
+plot(
+  x0, y0, asp = 1, pch = 19, xlab = "x", ylab = "y"
+)
+arrows(x0, y0, x1, y1, length = 0.1)
