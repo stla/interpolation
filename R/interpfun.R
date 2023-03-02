b5e75f3d83b6ad564b2af2d7ceba000af390de80
Stéphane Laurent <laurent_step@yahoo.fr>
2023-03-02 18:06:37 +0100

    doc & description

--- R/interpfun.R
+++ R/interpfun.R
@@ -3,9 +3,11 @@
 #'   function values at some given \code{(x,y)}-coordinates. 
 #'
 #' @param x,y two numeric vectors of the same size
-#' @param z a nummeric vector of the same size as \code{x} and \code{y}
+#' @param z a numeric vector or matrix of the same size as \code{x} and 
+#'   \code{y}, with two or three columns if it is a matrix
 #' @param method method of interpolation, either \code{"linear"} or 
-#'   \code{"sibson"}
+#'   \code{"sibson"}; the \code{"sibson"} method is not available for 
+#'   vector-valued functions, i.e. if \code{z} is a matrix
 #'
 #' @return A function whose graph interpolates the points \code{((x,y), z)}. 
 #' @export
@@ -32,6 +34,35 @@
 #' ( znew <- fun(xnew, ynew) )
 #' # true values:
 #' a + bx*xnew + by*ynew
+#' 
+#' # a vector-valued example ####
+#' x <- y <- c(-5, -4, -3, -2, 2, 3, 4, 5)
+#' From <- as.matrix(expand.grid(x0 = x, y0 = y))
+#' f <- function(x0y0) {
+#'   d <- c(-10, -5) - x0y0
+#'   x0y0 + 0.8 * d / sqrt(c(crossprod(d)))
+#' }
+#' To <- t(apply(From, 1L, f))
+#' x0 <- From[, "x0"]; y0 <- From[, "y0"]
+#' x1 <- To[, 1L]; y1 <- To[, 2L]
+#' # plot data
+#' plot(
+#'   x0, y0, asp = 1, pch = 19, xlab = "x", ylab = "y"
+#' )
+#' arrows(x0, y0, x1, y1, length = 0.1)
+#' # interpolate
+#' library(interpolation)
+#' fun <- interpfun(x0, y0, To, method = "linear")
+#' From_new <- rbind(
+#'   as.matrix(expand.grid(x0 = c(-1, 0, 1), y0 = (-5):5)),
+#'   as.matrix(expand.grid(x0 = c(-5, -4, -3, -2), y0 = c(-1, 0, 1))),
+#'   as.matrix(expand.grid(x0 = c(2, 3, 4, 5), y0 = c(-1, 0, 1)))
+#' )
+#' To_new   <- fun(From_new)
+#' x0 <- From_new[, "x0"]; y0 <- From_new[, "y0"]
+#' x1 <- To_new[, 1L]; y1 <- To_new[, 2L]
+#' points(x0, y0, pch = 19, col = "red")
+#' arrows(x0, y0, x1, y1, length = 0.1, col = "red")
 interpfun <- function(x, y, z, method = "linear") {
   method <- match.arg(method, c("linear", "sibson"))
   if(is.matrix(z)) {
@@ -48,13 +79,23 @@ interpfun <- function(x, y, z, method = "linear") {
       out <- function(xnew, ynew) {
         interpolate_linear(delxyz, rbind(xnew, ynew))
       }
-    } else {
+    } else if(nrow(XYZ) == 4L) {
       delxyzz <- delaunayXYZZ_linear(XYZ)
       out <- function(XYnew) {
         interpolate_linear2(delxyzz, t(XYnew))
       }
+    } else if(nrow(XYZ) == 5L) {
+      delxyzzz <- delaunayXYZZZ_linear(XYZ)
+      out <- function(XYnew) {
+        interpolate_linear3(delxyzzz, t(XYnew))
+      }
+    } else {
+      stop("Something wrong regarding x, y, z.")
     }
   } else {
+    if(nrow(XYZ) > 3L) {
+      stop("Sibson interpolation is not available for vector-valued functions.")
+    }
     delxyz <- delaunayXYZ_sibson(XYZ)
     out <- function(xnew, ynew) {
       interpolate_sibson(delxyz, rbind(xnew, ynew))
--- inst/essais/vector2-Field.R
+++ inst/essais/vector2-Field.R
@@ -3,31 +3,24 @@ From <- as.matrix(expand.grid(x0 = x, y0 = y))
 f <- function(x0y0) {
   d <- c(-10, -5) - x0y0
   x0y0 + 0.8 * d / sqrt(c(crossprod(d)))
-  # x0 <- x0y0[1L]; y0 <- x0y0[2L]
-  # x1 <- -x0/d; y1 <- -y0/d
-  # c(x1, y1)
 }
 To <- t(apply(From, 1L, f))
 x0 <- From[, "x0"]; y0 <- From[, "y0"]
 x1 <- To[, 1L]; y1 <- To[, 2L]
-
+# plot data
 plot(
   x0, y0, asp = 1, pch = 19, xlab = "x", ylab = "y"
 )
 arrows(x0, y0, x1, y1, length = 0.1)
-
-
+# interpolate
 library(interpolation)
-
 fun <- interpfun(x0, y0, To, method = "linear")
-
 From_new <- rbind(
   as.matrix(expand.grid(x0 = c(-1, 0, 1), y0 = (-5):5)),
   as.matrix(expand.grid(x0 = c(-5, -4, -3, -2), y0 = c(-1, 0, 1))),
   as.matrix(expand.grid(x0 = c(2, 3, 4, 5), y0 = c(-1, 0, 1)))
 )
 To_new   <- fun(From_new)
-
 x0 <- From_new[, "x0"]; y0 <- From_new[, "y0"]
 x1 <- To_new[, 1L]; y1 <- To_new[, 2L]
 points(x0, y0, pch = 19, col = "red")
