5f28fc3d160affd0c503e076f358b0e65f584622
Stéphane Laurent <laurent_step@yahoo.fr>
2023-03-02 04:36:50 +0100

    use matrices

--- inst/essais/essai01.R
+++ inst/essais/essai01.R
@@ -1,14 +1,21 @@
 library(interpolation)
 
+delaunayXYZ <- function(x, y, z) {
+  delaunayXYZ_cpp(rbind(x, y, z))
+}
+interpolate <- function(delxyz, xnew, ynew) {
+  interpolate_linear(delxyz, rbind(xnew, ynew))
+}
+
+
 a <- 0.2; bx <- 0.3; by <- -0.4
 x <- y <- seq(1, 10, by = 1)
 y <- y + 6*rnorm(10)
 z <- a + bx*x + by*y
+xnew <- ynew <- seq(3, 12, by = 1)
 
 delxyz <- delaunayXYZ(x, y, z)
-
-xnew <- ynew <- seq(3, 12, by = 1)
-znew <- test2(delxyz, xnew, ynew)
+znew <- interpolate(delxyz, xnew, ynew)
 
 znew
 a + bx*xnew + by*ynew
--- src/linear.cpp
+++ src/linear.cpp
@@ -41,16 +41,16 @@ Rcpp::NumericVector test(Rcpp::NumericVector x,
 }
 
 // [[Rcpp::export]]
-Rcpp::XPtr<std::pair<Delaunay2, Coord_map>> delaunayXYZ(Rcpp::NumericVector x,
-                                                        Rcpp::NumericVector y,
-                                                        Rcpp::NumericVector z) {
+Rcpp::XPtr<std::pair<Delaunay2, Coord_map>> delaunayXYZ_cpp(
+    Rcpp::NumericMatrix XYZ) {
   Delaunay2 T;
   Coord_map value_function;
-  int npoints = x.size();
+  int npoints = XYZ.ncol();
   for(int i = 0; i < npoints; i++) {
-    Point2 p(x(i), y(i));
+    Rcpp::NumericVector xyz = XYZ(Rcpp::_, i);
+    Point2 p(xyz(0), xyz(1));
     T.insert(p);
-    value_function.insert(std::make_pair(p, z(i)));
+    value_function.insert(std::make_pair(p, xyz(2)));
   }
 
   std::pair<Delaunay2, Coord_map> out = std::make_pair(T, value_function);
@@ -59,20 +59,21 @@ Rcpp::XPtr<std::pair<Delaunay2, Coord_map>> delaunayXYZ(Rcpp::NumericVector x,
 }
 
 // [[Rcpp::export]]
-Rcpp::NumericVector test2(Rcpp::XPtr<std::pair<Delaunay2, Coord_map>> xptr,
-                          Rcpp::NumericVector xnew,
-                          Rcpp::NumericVector ynew) {
+Rcpp::NumericVector interpolate_linear(
+    Rcpp::XPtr<std::pair<Delaunay2, Coord_map>> xptr,
+    Rcpp::NumericMatrix XYnew) {
   typedef CGAL::Data_access<Coord_map> Value_access;
   std::pair<Delaunay2, Coord_map> stuff = *(xptr.get());
   Delaunay2 T = stuff.first;
   Coord_map value_function = stuff.second;
 
   // coordinate computation
-  int nnewpoints = xnew.size();
+  int nnewpoints = XYnew.ncol();
   Rcpp::NumericVector znew(nnewpoints);
   for(int i = 0; i < nnewpoints; i++) {
     Rcpp::Rcout << i << " - ";
-    Point2 p(xnew(i), ynew(i));
+    Rcpp::NumericVector xynew = XYnew(Rcpp::_, i);
+    Point2 p(xynew(0), xynew(1));
     std::vector<std::pair<Point2, Coord_type>> coords;
     CGAL::Triple nnc =
         CGAL::natural_neighbor_coordinates_2(T, p, std::back_inserter(coords));
