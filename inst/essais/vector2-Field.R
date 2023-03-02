c9287580db6ed3abb8481f66c998ccbc5e70bcb0
Stéphane Laurent <laurent_step@yahoo.fr>
2023-03-02 17:38:01 +0100

    vector field example

--- inst/essais/vector2-Field.R
+++ inst/essais/vector2-Field.R
@@ -1,8 +1,8 @@
 x <- y <- c(-5, -4, -3, -2, 2, 3, 4, 5)
 From <- as.matrix(expand.grid(x0 = x, y0 = y))
 f <- function(x0y0) {
-  d <- 1 * sqrt(c(crossprod(x0y0)))
-  x0y0 - x0y0 / d
+  d <- c(-10, -5) - x0y0
+  x0y0 + 0.8 * d / sqrt(c(crossprod(d)))
   # x0 <- x0y0[1L]; y0 <- x0y0[2L]
   # x1 <- -x0/d; y1 <- -y0/d
   # c(x1, y1)
@@ -21,7 +21,11 @@ library(interpolation)
 
 fun <- interpfun(x0, y0, To, method = "linear")
 
-From_new <- as.matrix(expand.grid(x0 = c(-1, 0, 1), y0 = c(-1, 0, 1)))
+From_new <- rbind(
+  as.matrix(expand.grid(x0 = c(-1, 0, 1), y0 = (-5):5)),
+  as.matrix(expand.grid(x0 = c(-5, -4, -3, -2), y0 = c(-1, 0, 1))),
+  as.matrix(expand.grid(x0 = c(2, 3, 4, 5), y0 = c(-1, 0, 1)))
+)
 To_new   <- fun(From_new)
 
 x0 <- From_new[, "x0"]; y0 <- From_new[, "y0"]
